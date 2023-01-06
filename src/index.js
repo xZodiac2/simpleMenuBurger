a3a25ed78a2a647ff0cee929975f1d36020fc0f6
xZodiac2 <xirenoa@gmail.com>
2023-01-06 19:14:42 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+import домик from './images/дом.png'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.refToMenuContainer = React.createRef()
+        this.refToMenuButton = React.createRef()
+        this.state = {
+            offset: 0,
+            rotateDegress: 0
+        }
+    }
+
+    menuContainerWidth = 0
+    menuButton = 0
+
+
+    componentDidMount = () => {
+        this.menuContainerWidth = this.refToMenuContainer.current.clientWidth
+        this.menuButton = this.refToMenuButton.current
+    }
+
+    doMenuIsOpen = false
+
+    openMenu = () => {
+        if (this.doMenuIsOpen === false) {
+            this.doMenuIsOpen = true
+            this.setState(() => {
+                return {
+                    offset: this.menuContainerWidth,
+                    rotateDegress: 0
+                }
+            })
+        } else if (this.doMenuIsOpen === true) {
+            this.doMenuIsOpen = false
+            this.setState(() => {
+                return {
+                    offset: 0,
+                    rotateDegress: 90
+                }
+            })
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className="container">
+                    <div className="menu-burger">
+                        <div className="button" onClick={ this.openMenu } style={{ transform: `rotate(${this.state.rotateDegress}deg)` }}>
+                            <p>&mdash;</p>
+                            <p>&mdash;</p>
+                            <p>&mdash;</p>
+                        </div>
+                        <div className="menu-container" ref={ this.refToMenuContainer }>
+                            <div className="menu" style={{ right: this.state.offset }}>
+                                <div className="menu-item">
+                                    <img src={ домик } alt="" />
+                                </div>
+                                <div className="menu-item">
+                                    <img src={ домик } alt="" />
+                                </div>
+                                <div className="menu-item">
+                                    <img src={ домик } alt="" />
+                                </div>
+                                <div className="menu-item">
+                                    <img src={ домик } alt="" />
+                                </div>
+                                <div className="menu-item">
+                                    <img src={ домик } alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('.App')
+)
